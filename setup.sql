6fed15c5287d0ce83cdde41be6a653d7f757ef11
Jan Felix van Son <41387227+janfelixvs@users.noreply.github.com>
2023-12-28 11:44:22 +0100

    Final Database Version

--- setup.sql
+++ setup.sql
@@ -11,66 +11,52 @@ DROP TABLE IF EXISTS car;
 
 CREATE TABLE car (
     carID INT AUTO_INCREMENT PRIMARY KEY,
-    license_plate VARCHAR(255) NULL,
-    kilometerstand INT NOT NULL,
-    brand VARCHAR(255) NULL,
-    model VARCHAR(255) NULL,
-    reserved BIT NOT NULL
+    license_plate VARCHAR(255) ,
+    mileage INT ,
+    brand VARCHAR(255) ,
+    model VARCHAR(255) 
 );
 
 CREATE TABLE customer (
     customerID INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255) NOT NULL,
-    birth_date DATE NOT NULL,
-    driver_license_number VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    phone_number VARCHAR(255) NOT NULL,
-    payment_information TEXT NOT NULL
+    first_name VARCHAR(255) ,
+    last_name VARCHAR(255)  ,
+    email VARCHAR(255)   UNIQUE,
+    password VARCHAR(255)  
 );
 
 CREATE TABLE reservation (
     reservationID INT AUTO_INCREMENT PRIMARY KEY,
-    start_date DATETIME NOT NULL,
-    end_date DATETIME NOT NULL,
-    customerID INT NOT NULL,
-    carID INT NOT NULL,
+    start_date DATETIME  ,
+    end_date DATETIME  ,
+    customerID INT  ,
+    carID INT  ,
     FOREIGN KEY (customerID) REFERENCES customer(customerID),
     FOREIGN KEY (carID) REFERENCES car(carID)
 );
 
 CREATE TABLE maintenance (
     maintenanceID INT AUTO_INCREMENT PRIMARY KEY,
-    carID INT NOT NULL,
-    start_date DATETIME NOT NULL,
+    carID INT  ,
+    start_date DATETIME,
     end_date DATETIME,
-    status VARCHAR(255) DEFAULT 'scheduled',
     FOREIGN KEY (carID) REFERENCES car(carID)
 );
 
 CREATE TABLE employee (
     employeeID INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    position VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL
+    first_name VARCHAR(255)  ,
+    last_name VARCHAR(255)  ,
+    email VARCHAR(255)   UNIQUE,
+    password VARCHAR(255) 
 );
 
-CREATE TABLE notification (
-    notificationID INT AUTO_INCREMENT PRIMARY KEY,
-    customerID INT NOT NULL,
-    message TEXT NOT NULL,
-    sent_date DATETIME NOT NULL,
-    FOREIGN KEY (customerID) REFERENCES customer(customerID)
-);
 
 CREATE TABLE gps_tracking (
     trackingID INT AUTO_INCREMENT PRIMARY KEY,
-    carID INT NOT NULL,
-    timestamp DATETIME NOT NULL,
-    location TEXT NOT NULL,
+    carID INT,
+    timestamp DATETIME ,
+    location TEXT ,
     FOREIGN KEY (carID) REFERENCES car(carID)
 );
 
