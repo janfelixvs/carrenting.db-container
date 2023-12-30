980c95cf582548562ab6902ef6132ea848701010
Drolt <thomas_klug@gmx.de>
2023-12-30 12:47:47 +0100

    naming convention working (foreign_key_checks=0)

--- setup.sql
+++ setup.sql
@@ -18,7 +18,7 @@ CREATE TABLE car (
 );
 
 CREATE TABLE customer (
-    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    customerId INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(255) ,
     last_name VARCHAR(255)  ,
     email VARCHAR(255)   UNIQUE,
