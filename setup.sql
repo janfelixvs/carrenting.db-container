a60bc6e85b056ad8e2bed1391f356d8881bf4522
Drolt <thomas_klug@gmx.de>
2024-01-12 10:19:02 +0100

    reformated setup

--- setup.sql
+++ setup.sql
@@ -9,57 +9,63 @@ DROP TABLE IF EXISTS reservation;
 DROP TABLE IF EXISTS customer;
 DROP TABLE IF EXISTS car;
 
-CREATE TABLE car (
-    carID INT AUTO_INCREMENT PRIMARY KEY,
-    license_plate VARCHAR(255) ,
-    mileage INT ,
-    brand VARCHAR(255) ,
-    model VARCHAR(255) 
+CREATE TABLE car
+(
+    carID         INT AUTO_INCREMENT PRIMARY KEY,
+    license_plate VARCHAR(255),
+    mileage       INT,
+    brand         VARCHAR(255),
+    model         VARCHAR(255)
 );
 
-CREATE TABLE customer (
+CREATE TABLE customer
+(
     customerId INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(255) ,
-    last_name VARCHAR(255)  ,
-    email VARCHAR(255)   UNIQUE,
-    password VARCHAR(255)  
+    first_name VARCHAR(255),
+    last_name  VARCHAR(255),
+    email      VARCHAR(255) UNIQUE,
+    password   VARCHAR(255)
 );
 
-CREATE TABLE reservation (
+CREATE TABLE reservation
+(
     reservationID INT AUTO_INCREMENT PRIMARY KEY,
-    start_date DATETIME  ,
-    end_date DATETIME  ,
-    customerID INT  ,
-    carID INT  ,
-    FOREIGN KEY (customerID) REFERENCES customer(customerID),
-    FOREIGN KEY (carID) REFERENCES car(carID)
+    start_date    DATETIME,
+    end_date      DATETIME,
+    customerID    INT,
+    carID         INT,
+    FOREIGN KEY (customerID) REFERENCES customer (customerID),
+    FOREIGN KEY (carID) REFERENCES car (carID)
 );
 
-CREATE TABLE maintenance (
+CREATE TABLE maintenance
+(
     maintenanceID INT AUTO_INCREMENT PRIMARY KEY,
-    carID INT  ,
-    start_date DATETIME,
-    end_date DATETIME,
-    status VARCHAR(255) ,
-    FOREIGN KEY (carID) REFERENCES car(carID)
+    carID         INT,
+    start_date    DATETIME,
+    end_date      DATETIME,
+    status        VARCHAR(255),
+    FOREIGN KEY (carID) REFERENCES car (carID)
 );
 
-CREATE TABLE employee (
+CREATE TABLE employee
+(
     employeeID INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(255)  ,
-    last_name VARCHAR(255)  ,
-    email VARCHAR(255)   UNIQUE,
-    password VARCHAR(255) 
+    first_name VARCHAR(255),
+    last_name  VARCHAR(255),
+    email      VARCHAR(255) UNIQUE,
+    password   VARCHAR(255)
 );
 
 
-CREATE TABLE gps_tracking (
+CREATE TABLE gps_tracking
+(
     trackingID INT AUTO_INCREMENT PRIMARY KEY,
-    carID INT,
-    timestamp DATETIME ,
-    location TEXT ,
-    FOREIGN KEY (carID) REFERENCES car(carID) ON DELETE CASCADE
+    carID      INT,
+    timestamp  DATETIME,
+    location   TEXT,
+    FOREIGN KEY (carID) REFERENCES car (carID) ON DELETE CASCADE
 );
 
-SET GLOBAL FOREIGN_KEY_CHECKS=0;
+SET GLOBAL FOREIGN_KEY_CHECKS = 0;
 
